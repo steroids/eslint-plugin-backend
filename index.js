f4025caff3f960253fbdac68e4abbb9880e37b08
WereEz <denis03122002@mail.ru>
2026-05-07 15:14:13 +0700

    feat: add require-enum-field-enum-name rule for EnumField decorator (#2)
    * feat: add require-enum-field-enum-name rule for EnumField decorator
    
    * fix: limit EnumField rule to @steroidsjs/nest, remove spread handling
    
    * refactor: replace internal @typescript-eslint path with string literals
    
    ---------
    
    Co-authored-by: Denis <dpoyarkov@team.kozhin.dev>

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const requiredEnumToHelperRule = require("./lib/rules/required-enum-to-helper");
+const requiredEnumFieldEnumNameRule = require("./lib/rules/required-enum-field-enum-name");
+
 const plugin = {
     rules: {
         "required-enum-to-helper": requiredEnumToHelperRule,
-    } ,
+        "required-enum-field-enum-name": requiredEnumFieldEnumNameRule,
+    },
 };
 
 module.exports = plugin;
